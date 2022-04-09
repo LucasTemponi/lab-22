bee35908d36da46566a346d776e4534321ab4162
LucasTemponi <lucastmpn@gmail.com>
2022-04-08 23:32:39 -0300

    Implementa funções de incrementar e decrementar itens.

--- src/components/Incrementor/index.tsx
+++ src/components/Incrementor/index.tsx
@@ -1,5 +1,6 @@
 import { Plus as PlusIcon } from "@styled-icons/boxicons-regular/Plus";
 import { Subtract as SubtractIcon } from "@styled-icons/remix-fill/Subtract";
+import { InsertEmoticon } from "styled-icons/material-sharp";
 import { useCart } from "../../hooks/useCart";
 
 
@@ -12,14 +13,19 @@ type IncrementorProps = {
 
 const Incrementor = ({ id, quantity }: IncrementorProps) => {
   const incrementaItem = useCart(state=>state.incrementaItem)
+  const decrementaItem = useCart(state=>state.decrementaItem)
+  const quantidadeItem = useCart(state=>{
+    const item = state.items.find(element=>element.id===id)
+    return item?.quantity
+  })
   
   return (
       <Wrapper>
       <IconWrapper>
-        <SubtractIcon aria-label="Subtract item" />
+        <SubtractIcon onClick={()=>decrementaItem({id,quantity})} aria-label="Subtract item" />
       </IconWrapper>
 
-      <Quantity>{quantity}</Quantity>
+      <Quantity>{quantidadeItem ? quantidadeItem : 0 }</Quantity>
 
       <IconWrapper>
         <PlusIcon onClick={()=>incrementaItem({id,quantity})} aria-label="Add item" />
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -3,15 +3,28 @@ import { ProductProps } from '../components/Product';
 
 interface cartProps{
     items: {id:number,quantity:number}[],
-    incrementaItem:(novoItem:{id:number,quantity:number})=>void;
+    incrementaItem:(novoItem:{id:number,quantity:number})=>void,
+    decrementaItem:(novoItem:{id:number,quantity:number})=>void,
 }
 
 export const useCart = create<cartProps>((set)=> ({
     items:[],
     incrementaItem:(novoItem)=>set(({items})=>{
-            items.push(novoItem) 
-            console.log(items)  
-        })
-    }),
+            let checkForItem = items.find(element=>element.id===novoItem.id)
+            if (checkForItem){
+                checkForItem.quantity+=1
+                console.log(items)
+            }else{
+                items.push(novoItem)
+            } 
+        }),
+        decrementaItem:(novoItem)=>set(({items})=>{
+            let checkForItem = items.find(element=>element.id===novoItem.id)
+            if (checkForItem && checkForItem.quantity>0 ){
+                checkForItem.quantity-=1
+                console.log(items)
+            } 
+        }),
+    })
 )
 
